867bc572dd1252071e595d847b6a843915853a5f
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-15 10:47:40 -0400

    added scene structure

--- src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
+++ src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
@@ -4,14 +4,32 @@
  */
 package housewithwindowpaneslab;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Jacques-Conrad Calagui-Painchaud 6298805
  * 2025/10/15
+ * https://github.com/waffleprinter/HouseWithWindowPanesLab.git
  */
 public class HouseWithWindowPanesLab extends Application {
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
+    @Override
+    public void start(Stage stage) {
+        final double SCENE_WIDTH = 500;
+        final double SCENE_HEIGHT = 500;
+        
+        Pane root = new Pane();
+        
+        Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
 }
