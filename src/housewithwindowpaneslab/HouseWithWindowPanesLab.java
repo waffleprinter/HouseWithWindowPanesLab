34b12271ee13df531a8f483b87594dd2fb4abbfb
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-15 10:43:50 -0400

    initial commit

---
+++ src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package housewithwindowpaneslab;
+
+/**
+ *
+ * @author Jacques-Conrad Calagui-Painchaud 6298805
+ * 2025/10/15
+ */
+public class HouseWithWindowPanesLab extends Application {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
