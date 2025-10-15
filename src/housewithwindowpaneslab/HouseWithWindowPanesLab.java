cff41e6d36bfc75a7743e5b89f39bffee327a7de
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-15 11:38:22 -0400

    added windows with lines

--- src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
+++ src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
@@ -8,6 +8,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -40,6 +41,28 @@ public class HouseWithWindowPanesLab extends Application {
         
         final double DOOR_WIDTH = 50, DOOR_HEIGHT = 100;
         
+        final double WINDOW_WIDTH = 50, WINDOW_HEIGHT = 50;
+        final double WINDOW_HORIZONTAL_OFFSET = 30;
+        final double WINDOW1_X = WALLS_X + WINDOW_HORIZONTAL_OFFSET;
+        final double WINDOW2_X = WALLS_X + WALLS_WIDTH - WINDOW_WIDTH - WINDOW_HORIZONTAL_OFFSET;
+        final double WINDOW_Y = GRASS_Y - DOOR_HEIGHT - WINDOW_HEIGHT;
+        
+        final double LINE1_X1 = WINDOW1_X;
+        final double LINE1_X2 = WINDOW1_X + WINDOW_WIDTH;
+        final double LINE1_Y = WINDOW_Y + WINDOW_HEIGHT / 2;
+        
+        final double LINE2_X = WINDOW1_X + WINDOW_WIDTH / 2;
+        final double LINE2_Y1 = WINDOW_Y;
+        final double LINE2_Y2 = WINDOW_Y + WINDOW_HEIGHT;
+        
+        final double LINE3_X1 = WINDOW2_X;
+        final double LINE3_X2 = WINDOW2_X + WINDOW_WIDTH;
+        final double LINE3_Y = WINDOW_Y + WINDOW_HEIGHT / 2;
+        
+        final double LINE4_X = WINDOW2_X + WINDOW_WIDTH / 2;
+        final double LINE4_Y1 = WINDOW_Y;
+        final double LINE4_Y2 = WINDOW_Y + WINDOW_HEIGHT;
+        
         Rectangle grass = new Rectangle(GRASS_X, GRASS_Y, SCENE_WIDTH, GRASS_HEIGHT);
         grass.setFill(Color.GREEN);
         
@@ -60,9 +83,21 @@ public class HouseWithWindowPanesLab extends Application {
         Rectangle door = new Rectangle(WALLS_X + WALLS_WIDTH / 2 - DOOR_WIDTH / 2, GRASS_Y - DOOR_HEIGHT, DOOR_WIDTH, DOOR_HEIGHT);
         door.setFill(Color.BROWN);
         
+        Rectangle window1 = new Rectangle(WINDOW1_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        window1.setFill(Color.LIGHTBLUE);
+        
+        Line line1 = new Line(LINE1_X1, LINE1_Y, LINE1_X2, LINE1_Y);
+        Line line2 = new Line(LINE2_X, LINE2_Y1, LINE2_X, LINE2_Y2);
+        
+        Rectangle window2 = new Rectangle(WINDOW2_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
+        window2.setFill(Color.LIGHTBLUE);
+        
+        Line line3 = new Line(LINE3_X1, LINE3_Y, LINE3_X2, LINE3_Y);
+        Line line4 = new Line(LINE4_X, LINE4_Y1, LINE4_X, LINE4_Y2);
+        
         Pane root = new Pane();
         root.getChildren().addAll(
-                grass, walls, doorstep, roof, door
+                grass, walls, doorstep, roof, door, window1, window2, line1, line2, line3, line4
         );
         
         Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
