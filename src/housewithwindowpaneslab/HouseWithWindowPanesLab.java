c087dd5808e473e2cf77d4f4b101ddfbcc7a9ff8
waffleprinter <112639491+waffleprinter@users.noreply.github.com>
2025-10-15 11:17:10 -0400

    added door + refactored some constants

--- src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
+++ src/housewithwindowpaneslab/HouseWithWindowPanesLab.java
@@ -29,34 +29,40 @@ public class HouseWithWindowPanesLab extends Application {
         final double SCENE_HEIGHT = 500;
         
         final double GRASS_HEIGHT = 50;
+        final double GRASS_X = 0, GRASS_Y = SCENE_HEIGHT - GRASS_HEIGHT;
         
-        final double WALLS_X = 125;
         final double WALLS_WIDTH = 250, WALLS_HEIGHT = 200;
+        final double WALLS_X = 125, WALLS_Y = GRASS_Y - WALLS_HEIGHT;
         
         final double DOORSTEP_HEIGHT = 10;
         
         final double ROOF_HEIGHT = 120;
         
-        Rectangle grass = new Rectangle(0, SCENE_HEIGHT - GRASS_HEIGHT, SCENE_WIDTH, GRASS_HEIGHT);
+        final double DOOR_WIDTH = 50, DOOR_HEIGHT = 100;
+        
+        Rectangle grass = new Rectangle(GRASS_X, GRASS_Y, SCENE_WIDTH, GRASS_HEIGHT);
         grass.setFill(Color.GREEN);
         
-        Rectangle walls = new Rectangle(WALLS_X, SCENE_HEIGHT - GRASS_HEIGHT - WALLS_HEIGHT, WALLS_WIDTH, WALLS_HEIGHT);
+        Rectangle walls = new Rectangle(WALLS_X, WALLS_Y, WALLS_WIDTH, WALLS_HEIGHT);
         walls.setFill(Color.LIGHTGRAY);
         walls.setStroke(Color.BLACK);
         
-        Rectangle doorstep = new Rectangle(WALLS_X, SCENE_HEIGHT - GRASS_HEIGHT, WALLS_WIDTH, DOORSTEP_HEIGHT);
+        Rectangle doorstep = new Rectangle(WALLS_X, GRASS_Y, WALLS_WIDTH, DOORSTEP_HEIGHT);
         doorstep.setFill(Color.BEIGE);
         
         Polygon roof = new Polygon(
-                WALLS_X, SCENE_HEIGHT - GRASS_HEIGHT - WALLS_HEIGHT,
-                WALLS_X + WALLS_WIDTH, SCENE_HEIGHT - GRASS_HEIGHT - WALLS_HEIGHT,
-                WALLS_X + WALLS_WIDTH / 2, SCENE_HEIGHT - GRASS_HEIGHT - WALLS_HEIGHT - ROOF_HEIGHT
+                WALLS_X, WALLS_Y,
+                WALLS_X + WALLS_WIDTH, WALLS_Y,
+                WALLS_X + WALLS_WIDTH / 2, WALLS_Y - ROOF_HEIGHT
         );
         roof.setFill(Color.RED);
         
+        Rectangle door = new Rectangle(WALLS_X + WALLS_WIDTH / 2 - DOOR_WIDTH / 2, GRASS_Y - DOOR_HEIGHT, DOOR_WIDTH, DOOR_HEIGHT);
+        door.setFill(Color.BROWN);
+        
         Pane root = new Pane();
         root.getChildren().addAll(
-                grass, walls, doorstep, roof
+                grass, walls, doorstep, roof, door
         );
         
         Scene scene = new Scene(root, SCENE_WIDTH, SCENE_HEIGHT);
